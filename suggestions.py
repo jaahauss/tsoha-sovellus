c8609d2e11c0e1c923660b7f85ffa81edf17de1c
Jaana Haussalo <145219384+jaahauss@users.noreply.github.com>
2024-06-08 15:28:31 +0300

    Corrected a mistake in suggestions.py

--- suggestions.py
+++ suggestions.py
@@ -3,10 +3,11 @@ from flask import session
 from sqlalchemy.sql import text
 
 def suggest(content):
-    sql = text("INSERT INTO suggestions (content) VALUES (:content)")
-    db.session.execute(sql, {"content":content})
-    sql = text("UPDATE suggestions SET visible=TRUE WHERE id=:id")
-    db.session.execute(sql, {"id":id})
+    sql = text("INSERT INTO suggestions (content) VALUES (:content) RETURNING id")
+    result = db.session.execute(sql, {"content":content})
+    suggestion_id = result.fetchone()[0]
+    sql = text("UPDATE suggestions SET visible=TRUE WHERE id=:suggestion_id")
+    db.session.execute(sql, {"suggestion_id":suggestion_id})
     db.session.commit()
     
 def delete(id):
